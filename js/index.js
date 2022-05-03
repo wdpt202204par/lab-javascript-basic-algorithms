b3839611fc795f4cd5f41ad4417fe46c80fc33af
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-05-03 17:41:49 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
 // Iteration 1: Names and Input
+let driver = "Cécile";
+console.log("The car driver's name is, " + driver);
 
+let navigator = "Bérénice";
+console.log("The navigator name is, " + navigator);
 
 // Iteration 2: Conditionals
+function longestName(i);{
+    let result = '';
+    if ( driver.length < navigator.length);{
+      result = navigator;
+    } if ( driver.length > navigator.length); {
+      result = driver;
+    } else {
+      result = "Same letter number";
+    }
+  }
+  
+  console.log(longestName(${result}));
 
 
 // Iteration 3: Loops
+function upSep ();{
+    let upperCaseDriver = driver.toUpperCase;
+    let separateUppercase = upperCaseDriver.split(" ");
+    }
+    console.log(upSep);
+    
+    function revNav ();{
+    let reverseNavigator = navigator.split(" ").reverse().joint("");
+    }
+    console.log(revNav)
